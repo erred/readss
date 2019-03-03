416ed8024235fc7061e60f8a6da3200b5eefb4f8
Sean Liao <seankhliao@gmail.com>
2019-03-03 15:38:33 +0800

    update sw to cache-first

--- static/sw.js
+++ static/sw.js
@@ -1,37 +1,38 @@
 "use strict";
 
-var cacheVersion = 1;
-var currentCache = {
-  offline: "offline-cache" + cacheVersion
-};
-const offlineUrl = "/";
+const cacheName = "cache-1";
+const toCache = ["base.css"];
 
 this.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(currentCache.offline).then(function(cache) {
-      return cache.addAll([offlineUrl]);
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(toCache);
     })
   );
 });
 
-this.addEventListener("fetch", event => {
-  // request.mode = navigate isn't supported in all browsers
-  // so include a check for Accept: text/html header.
-  if (
-    event.request.mode === "navigate" ||
-    (event.request.method === "GET" && event.request.headers.get("accept").includes("text/html"))
-  ) {
-    event.respondWith(
-      fetch(event.request.url).catch(error => {
-        // Return the offline page
-        return caches.match(offlineUrl);
-      })
-    );
-  } else {
-    // Respond with everything else if we can
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  let update = true;
+  e.respondWith(
+    caches.open(cacheName).then(cache => {
+      return cache.match(e.request).then(res => {
+        return (
+          res ||
+          fetch(e.request).then(res => {
+            update = false;
+            cache.put(e.request, res.clone());
+            return res;
+          })
+        );
+      });
+    })
+  );
+  if (update) {
+    e.waitUntil(
+      caches.open(cacheName).then(cache => {
+        fetch(e.request).then(res => {
+          cache.put(e.request, res);
+        });
       })
     );
   }
